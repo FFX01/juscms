9be17a7890ba64c57b13d9197d8921fe51dde305
ffx01 <walters.justin01@gmail.com>
2016-01-16 20:13:07 -0800

    V0.0.2 - Updated comment documentation for pages/views.py

--- juscms/pages/views.py
+++ juscms/pages/views.py
@@ -6,6 +6,22 @@ from .models import Page
 
 class BaseView(View):
     def get(self, request, path):
+        """
+        This method is called when a client machine requests a url
+        that fits the url pattern to call basic content pages.
+
+        Parameters:
+            self(class): The class that calls this method. In this case it is
+                the baseview class.
+            request(object): The http request object as received by the server
+                the client.
+            path(string): The path captured in the request url as defined in
+                pages/urls.py. This parameter is used to retrieve a page
+                instance to render.
+
+        Returns(function): The render function used to pass variables to the
+            HTML template which is generated and sent to the client machine.
+        """
         instance = get_object_or_404(Page, path=path)
         context = {
             'instance': instance,
