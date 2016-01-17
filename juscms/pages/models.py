e3b8a116a985f21c1d570b39df8034cb5ff57b50
ffx01 <walters.justin01@gmail.com>
2016-01-16 19:50:03 -0800

    V0.0.2 - Updated comment documentation for pages/models.py

--- juscms/pages/models.py
+++ juscms/pages/models.py
@@ -8,6 +8,40 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Page(MPTTModel):
+    """
+    This is the basic page model. All user defined page models inherit from
+    this model.
+
+    Inherits From:
+        MPTTModel: This model is from the django-mptt package. This is what
+            allows Pages to be nested. This model uses Multi Preorder Tree
+            Traversal to build a reference tree for objects.
+
+    Attributes:
+        title(string): The page title. Used mainly for reference in the admin
+            interface.
+        slug(string): The URL slug version of the page name. This is used to
+            construct a URL path. The URL path is what is used to route the
+            main view to the correct object instance.
+        path(string): This is the 'Page' instance's URL path. This is generated
+            when saving the model for the first time and regenerated when the
+            'Page' instance's parent is changed.
+        seo_title(string): This is the Page instance title as it appears in the
+            HTML document. It appears between the '<title></title>' tags in the
+            base template.
+        seo_description(string): This is the Page instance meta description as
+            it appears in the HTML document. It appears in the
+            '<meta name="description">' tag.
+        parent(object): The Page instance parent Page instance. Pages can be
+            nested under other pages.
+        template(string): The path to the Django HTML template file. If a user
+            wants to change the layout of a page, they can either edit the
+            default file directly or define their own.
+        is_home(boolean): If set to true the page instance will be set as the
+            home page, the path attribute will be set to a blank string, and
+            the parent attribute will be set to None. Any other page that has
+            this attribute set to True will have is_home set to False.
+    """
     title = models.CharField(
         verbose_name='Page Title',
         blank=False,
@@ -62,6 +96,18 @@ class Page(MPTTModel):
         order_insertion_by = ['title']
 
     def build_path(self):
+        """
+        This method builds a string containing the Page instance URL path.
+
+        Variables:
+            path(string): Initialized as an empty string. Will be populated
+                with a string containing the Page instance URL path.
+            path_list(list): A list of this Page instance ancestors including
+                this Page instance itself.
+
+        Returns:
+            path(string): The formatted URL path of this Page instance.
+        """
         path = ''
         path_list = self.get_ancestors(include_self=True)
         for item in path_list:
@@ -69,12 +115,24 @@ class Page(MPTTModel):
         return path
 
     def get_absolute_url(self):
+        """
+        Generates the absolute URL of the Page instance. This is used in the
+        admin to enable the 'View on site' button.
+        """
         return reverse(
             'pages:base_view',
             kwargs={'path': self.path},
         )
 
     def save(self, *args, **kwargs):
+        """
+        This method overrides the default Django model save method in order
+        to conditionally modify the Page instance attributes. It generates the
+        Page instance slug and checks to see if the Page instance has it's
+        is_home attribute set to True. If so, it sets the Page instance
+        path attribute to an empty string, and the parent attribute to None. It
+        also checks if the slug attribute has changed and updates it accordingly.
+        """
         if not self.is_home:
             new_slug = slugify(self.title)
             if not self.slug:
@@ -103,6 +161,16 @@ class Page(MPTTModel):
 
 
 class HTMLContent(models.Model):
+    """
+    This is the base abstract model for HTML content objects. All HTML content
+    objects inherit from this class.
+
+    Attributes:
+        html_ids(string): This is the HTML id that will be applied to the HTML
+            tag in the DOM. Used for styling.
+        html_class(string): Same as the html_ids attribute except it populates
+            the HTML class attribute. Used for styling.
+    """
     html_ids = models.CharField(
         verbose_name='HTML ID',
         blank=True,
@@ -119,6 +187,16 @@ class HTMLContent(models.Model):
 
 
 class Row(HTMLContent):
+    """
+    Model used for generating HTML row objects.
+
+    Attributes:
+        parent(object): The parent page of this row. This is a one to many
+            relationship. A page instance can have multiple row instances as
+            children.
+        template(string): Path to the template file used to render this model
+            instance.
+    """
     parent = models.ForeignKey(
         Page,
         verbose_name='Parent Page',
@@ -148,6 +226,17 @@ class Row(HTMLContent):
 
 
 class Chunk(HTMLContent):
+    """
+    Model used for generating HTML chunk objects.
+
+    Attributes:
+        parent(object): The parent Row for this Chunk instance. All chunks
+            MUST have a parent Row.
+        template(string): The path to the HTML template used to render the
+            chunk instance.
+        content(test/HTML): The user entered HTML content inside of this chunk.
+            Will be rendered as part of the DOM.
+    """
     parent = models.ForeignKey(
         Row,
         verbose_name='Parent Row',
@@ -182,6 +271,11 @@ class Chunk(HTMLContent):
 
 @receiver(post_save, sender=Page)
 def update_path(sender, instance, **kwargs):
+    """
+    This function listens to the save signal sent by a Page instance after it
+    has been created or modified and performers some logic to make sure that it
+    is callable by the view and sits correctly into the page tree.
+    """
     if instance.is_home is False:
         if not instance.path:
             if instance.parent:
