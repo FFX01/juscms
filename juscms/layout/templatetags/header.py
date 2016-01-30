039895c4f855f90c8a24dc49a26dee2a10066c3a
ffx01 <walters.justin01@gmail.com>
2016-01-30 10:13:45 -0800

    V0.0.2 - updated header template tag. updated base.html to include header template tag. updated header.html

--- juscms/layout/templatetags/header.py
+++ juscms/layout/templatetags/header.py
@@ -6,5 +6,9 @@ from layout.models import Header
 register = template.Library()
 
 
-@register.inclusion_tag('footer.html')
-def render_header()
+@register.inclusion_tag('header.html')
+def render_header():
+    header = Header.objects.all()[:1]
+    return {
+        'header': header,
+    }
